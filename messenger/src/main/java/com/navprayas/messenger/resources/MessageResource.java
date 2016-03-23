c642432bc0590267a2860892fc3c5708966c1766
n83667 <n83667@NLLR4000288180.solon.prd>
2016-03-23 08:36:30 +0100

    commit Update and delete

--- messenger/src/main/java/com/navprayas/messenger/resources/MessageResource.java
+++ messenger/src/main/java/com/navprayas/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package com.navprayas.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,30 +16,42 @@ import com.navprayas.messenger.model.Message;
 import com.navprayas.messenger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 	
 	MessageService msgService=new MessageService();
 	
+	//Select
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages(){
 		return msgService.getAllMessages();
 	}
 	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
+	
+	@POST	
 	public Message addMessage(Message msg){
 		return msgService.addMessage(msg);
 	}
 	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId")long id,Message msg){
+		msg.setId(id);
+		return msgService.updateMessage(msg);
+	}
+	
+	
+	@DELETE
+	@Path("/{messageId}")
+	public Message removeMessage(@PathParam("messageId")long id){
+		return msgService.removeMessage(id);
+	}
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId")long id){
-		return msgService.getMessage(id);
-		
+		return msgService.getMessage(id);		
 	}
 	
 
